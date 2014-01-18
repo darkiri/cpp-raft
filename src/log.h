f999468ec8067538927065d27568df1be10475e8
darkiri <darkiri@gmail.com>
2014-01-18 22:23:08 +0100

    remove using std from header

--- src/log.h
+++ src/log.h
@@ -4,56 +4,54 @@
 #include <vector>
 #include <memory>
 
-using namespace std;
+    namespace raft {
+    struct LogEntry {
+      unsigned int term;
+  // TODO   void* data;
+    };
 
-namespace raft {
-  struct LogEntry {
-    unsigned int term;
-// TODO   void* data;
-  };
+    class Log {
+      public:
+        Log() {};
+        virtual ~Log() {};
 
-  class Log {
-    public:
-      Log() {};
-      virtual ~Log() {};
+        // the return value is available as long as the Log alive
+        virtual const LogEntry* GetLastEntry() const = 0;
+        virtual const LogEntry* Get(unsigned int index) const = 0;
+        virtual unsigned int Size() const = 0;
+        virtual void Append(const LogEntry& entry) = 0;
+        virtual void Trim(unsigned int index) = 0;
+      private:
+        Log(Log&);
+        Log& operator=(const Log&);
+    };
 
-      // the return value is available as long as the Log alive
-      virtual const LogEntry* GetLastEntry() const = 0;
-      virtual const LogEntry* Get(unsigned int index) const = 0;
-      virtual unsigned int Size() const = 0;
-      virtual void Append(const LogEntry& entry) = 0;
-      virtual void Trim(unsigned int index) = 0;
-    private:
-      Log(Log&);
-      Log& operator=(const Log&);
-  };
-
-  class InMemoryLog : public Log {
-    public:
-      InMemoryLog() {};
-      const LogEntry* GetLastEntry() const {
-        return entries_.empty()
-          ? nullptr
-          : entries_.back().get();
-      };
-      const LogEntry* Get(unsigned int index) const {
-        return entries_.size() <= index 
-          ? nullptr 
-          : entries_[index].get();
-      }
-      virtual unsigned int Size() const {
-        return entries_.size();
-      }
-      void Append(const LogEntry& entry) {
-        auto e = unique_ptr<LogEntry>(new LogEntry(entry));
-        entries_.push_back(move(e));
-      }
-      void Trim(unsigned int index) {
-        entries_.erase(entries_.begin() + index, entries_.end());
-      }
-      virtual ~InMemoryLog() { };
-    private:
-      vector<unique_ptr<LogEntry>> entries_;
-  };
-}
+    class InMemoryLog : public Log {
+      public:
+        InMemoryLog() {};
+        const LogEntry* GetLastEntry() const {
+          return entries_.empty()
+            ? nullptr
+            : entries_.back().get();
+        };
+        const LogEntry* Get(unsigned int index) const {
+          return entries_.size() <= index 
+            ? nullptr 
+            : entries_[index].get();
+        }
+        virtual unsigned int Size() const {
+          return entries_.size();
+        }
+        void Append(const LogEntry& entry) {
+          auto e = std::unique_ptr<LogEntry>(new LogEntry(entry));
+          entries_.push_back(move(e));
+        }
+        void Trim(unsigned int index) {
+          entries_.erase(entries_.begin() + index, entries_.end());
+        }
+        virtual ~InMemoryLog() { };
+      private:
+        std::vector<std::unique_ptr<LogEntry>> entries_;
+    };
+  }
 #endif
