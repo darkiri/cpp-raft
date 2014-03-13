c5057225b7eb035c3f10422ed113c97022fbc567
darkiri <darkiri@gmail.com>
2014-03-13 23:01:37 +0100

    dead-simple timeout generator

---
+++ src/timeout.h
@@ -0,0 +1,32 @@
+#ifndef TIMEOUT_RAFT
+#define TIMEOUT_RAFT
+
+#include <random>
+#include <chrono>
+
+namespace raft {
+  class Timeout{
+    public:
+      const unsigned int MIN = 150;
+      const unsigned int MAX = 300;
+
+      Timeout() {
+        rnd_.seed(std::chrono::system_clock::now().time_since_epoch().count());
+        Reset();
+      }
+
+      unsigned int Get(){
+        return t_;
+      }
+
+      // not thread-safe
+      void Reset(){
+        std::uniform_int_distribution<int> d(MIN, MAX);
+        t_ = d(rnd_);
+      }
+    private:
+      int t_;
+      std::default_random_engine rnd_;
+  };
+}
+#endif
